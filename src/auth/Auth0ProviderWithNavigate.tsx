b2bf4e9c6172628168c4bf65e5211839eed82823
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-01-30 19:05:13 +0530

    added audience -auth

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -11,8 +11,9 @@ const Auth0ProviderWithNavigate = ({children}:Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if(!domain || !clientId || !redirectUri){
+    if(!domain || !clientId || !redirectUri || !audience){
         throw new Error("unable to initialise auth");
         
     }
